faebafcbd6a2dee67a8a972fb9535b2c1a72b794
AQUA\supram.maharwantijo <supram.maharwantijo@cpp.co.id>
2023-04-16 17:18:45 +0700

    update

--- be_lumen/routes/web.php
+++ be_lumen/routes/web.php
@@ -16,3 +16,13 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/todo', 'todoController@index');
+
+$router->get('/todo/{id}', 'todoController@show');
+
+$router->post('/todo/save', 'todoController@store');
+
+$router->post('/todo/{id}/update', 'todoController@update');
+
+$router->post('/todo/{id}/delete', 'todoController@destroy');
