56f7a79faf0da11acafaf12e16a00f6be67ea25d
AQUA\supram.maharwantijo <supram.maharwantijo@cpp.co.id>
2023-04-16 19:49:42 +0700

    update

---
+++ be_08/app/Http/Controllers/MenuController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Menus\GetSidebarMenu;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $user = auth()->user();
+            if($user && !empty($user)){
+                $roles =  $user->menuroles;
+            }else{
+                $roles = '';
+            }
+        } catch (Exception $e) {
+            $roles = '';
+        }   
+        if($request->has('menu')){
+            $menuName = $request->input('menu');
+        }else{
+            $menuName = 'sidebar menu';
+        }
+        $menus = new GetSidebarMenu();
+        return response()->json( $menus->get( $roles, $menuName ) );
+    }
+
+    public function check(Request $request)
+    {
+       
+        try {
+            $user = auth()->user();
+            if($user && !empty($user)){
+                $roles =  $user->menuroles;
+                return response()->json(['status' => 'OK'],200);
+            }else{
+                $roles = '';
+            }
+        } catch (Exception $e) {
+            $roles = '';
+        }   
+        return response()->json(['status' => 'Not OK'], 402);
+    }
+
+}
+
---
+++ be_08/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::get('menu', 'MenuController@index');
+    Route::post('login', 'AuthController@login');
+    Route::post('loginx', 'AuthController@loginx');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('register', 'AuthController@register');
+    Route::get('check', 'MenuController@check');
+});
